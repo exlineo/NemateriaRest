76fcaa5a12faa78a10e1b76f9e3e6a575b1fa0e7
exlineo <gabbadie@step-sa.fr>
2019-02-28 16:07:30 +0100

    Ajout d'un fichier de configuration

---
+++ src/config-example.ts
@@ -0,0 +1,1 @@
+export const CONFIG_ADR = ''; // Server address
--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -1,12 +1,15 @@
 import * as mongoose from 'mongoose';
-import { DB_PROVIDER } from '../constants'; 
+import { DB_PROVIDER } from '../constants';
+import { CONFIG_ADR } from '../config';
 
 export const databaseProviders = [
     {
         provide: DB_PROVIDER,
+        adr:CONFIG_ADR,
         useFactory: async () => {
             (mongoose as any).Promise = global.Promise;
-            return await mongoose.connect('mongodb://localhost:27017/');
+            return await mongoose.connect('mongodb://localhost:27017/nemateria');
+            // return await mongoose.connect(this.adr);
         },
     },
 ];
