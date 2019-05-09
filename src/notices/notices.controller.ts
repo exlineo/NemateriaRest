135bad060828ba2a8bbd1ad82db3d82a8d180708
exlineo <gabbadie@step-sa.fr>
2019-05-09 18:59:48 +0200

    Gestion des notices

--- src/notices/notices.controller.ts
+++ src/notices/notices.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, Query } from '@nestjs/common';
 import { CreateNoticeDto } from './dto/create-notice.dto';
 import { NoticesService } from './notices.service';
 import { NoticeModel } from './interfaces/notice.interface';
@@ -7,13 +7,52 @@ import { NoticeModel } from './interfaces/notice.interface';
 export class NoticesController {
     constructor(private readonly noticesService: NoticesService) { }
 
-    @Post()
-    async create(@Body() CreateNoticeDto: CreateNoticeDto) {
-        this.noticesService.create(CreateNoticeDto);
-    }
-
+    
+    /**
+     * Sélectionner toutes les notices une celles correspondant à la requête
+     * @param req Requête reçue dans le corps
+     */
     @Get()
-    async findAll(): Promise<NoticeModel[]> {
-        return this.noticesService.findAll();
+    async findReq(@Query() req):Promise<any> {
+        if(req){
+            console.log("requete notice détectée", req); // dir > le dossier à scanner, q > des variables passées
+            return this.noticesService.findReq(req);
+        }else{
+            console.log("Notices : pas de requete détectée");
+            return this.noticesService.findAll();
+        }
+    }
+    /**
+     * Récupérer une collection en fonction de son ID
+     * @param id ID de la collection à récupérer
+     */
+    @Get(':id')
+    async findUn(@Param('id') id): Promise<NoticeModel> {
+        return this.noticesService.findUn(id);
+    }
+    /**
+     * Créer une notice
+     * @param CreateNoticeDto Schéma de la notice à créer (reçu dans le body)
+     */
+    @Put()
+    async cree(@Body() CreateNoticeDto: CreateNoticeDto) {
+        this.noticesService.cree(CreateNoticeDto);
+    }
+    /**
+     * Mettre à jour une notice
+     * @param id ID de la notice à mettre à jour
+     * @param n Données de la notice mise à jour
+     */
+    @Post(':id')
+    async updateUn(@Param('id') id, @Body() n) {
+        this.noticesService.updateUn(id, n);
+    }
+    /**
+     * Supprimer une notice
+     * @param id ID de la notice à supprimer
+     */
+    @Delete(':id')
+    async deleteUn(@Param('id') id) {
+        this.noticesService.deleteUn(id);
     }
 }
--- src/notices/notices.service.ts
+++ src/notices/notices.service.ts
@@ -10,12 +10,40 @@ export class NoticesService {
     constructor(
         @Inject(NOTICES_MODEL_PROVIDER) private readonly noticeModel: Model<NoticeModel>) { }
 
-    async create(createNoticeDto: CreateNoticeDto): Promise<NoticeModel> {
+    async cree(createNoticeDto: CreateNoticeDto): Promise<NoticeModel> {
         const createdNotice = new this.noticeModel(createNoticeDto);
         return await createdNotice.save();
     }
-
+    /**
+     * Rechercher des données en fonction de critères transmis
+     * @param req Argument transmis pour faire une recherche
+     */
+    async findUn(id): Promise<any> {
+        return await this.noticeModel.find(id).exec();
+    }
+    /**
+     * Rechercher des données en fonction de critères transmis
+     * @param req Argument transmis pour faire une recherche
+     */
+    async findReq(req): Promise<NoticeModel[]> {
+        return await this.noticeModel.find(req).exec();
+    }
+    /**
+     * Trouver toutes les notices
+     */
     async findAll(): Promise<NoticeModel[]> {
         return await this.noticeModel.find().exec();
+    }
+    /**
+     * 
+     * @param id ID de la notice à mettre à jour
+     * @param n Les données de la notice modifiée
+     */
+    async updateUn(id, n){
+        return await this.noticeModel.updateOne(id, n).exec();
+    }
+
+    async deleteUn(id){
+        return await this.noticeModel.deleteOne(id).exec();
     }
 }
