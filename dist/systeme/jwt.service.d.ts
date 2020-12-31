a06168effeb809ea6210aeecf07dc19e6c6fb46c
exlineo <contact@exlineo.com>
2020-12-31 20:11:04 +0100

    Ajout de l'identification et d'un JWT

---
+++ dist/systeme/jwt.service.d.ts
@@ -0,0 +1,8 @@
+import { Model } from 'mongoose';
+import { CompteModel } from '../comptes/interfaces/compte.interface';
+export declare class JwtService {
+    private readonly compteModel;
+    token: any;
+    constructor(compteModel: Model<CompteModel>);
+    creeToken(email: any, statut: any): Promise<any>;
+}
---
+++ prod/main.a46be6d50f2e12e3c310.hot-update.js
@@ -0,0 +1,55 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "main";
+exports.ids = null;
+exports.modules = {
+
+/***/ "./src/comptes/comptes.controller.ts":
+/*!*******************************************!*\
+  !*** ./src/comptes/comptes.controller.ts ***!
+  \*******************************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements: top-level-this-exports, __webpack_exports__, __webpack_require__ */
+/*! CommonJS bailout: this is used directly at 2:18-22 */
+/*! CommonJS bailout: this is used directly at 8:18-22 */
+/*! CommonJS bailout: this is used directly at 11:15-19 */
+/*! CommonJS bailout: this is used directly at 14:17-21 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\nvar __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {\r\n    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;\r\n    if (typeof Reflect === \"object\" && typeof Reflect.decorate === \"function\") r = Reflect.decorate(decorators, target, key, desc);\r\n    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;\r\n    return c > 3 && r && Object.defineProperty(target, key, r), r;\r\n};\r\nvar __metadata = (this && this.__metadata) || function (k, v) {\r\n    if (typeof Reflect === \"object\" && typeof Reflect.metadata === \"function\") return Reflect.metadata(k, v);\r\n};\r\nvar __param = (this && this.__param) || function (paramIndex, decorator) {\r\n    return function (target, key) { decorator(target, key, paramIndex); }\r\n};\r\nvar __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {\r\n    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }\r\n    return new (P || (P = Promise))(function (resolve, reject) {\r\n        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }\r\n        function rejected(value) { try { step(generator[\"throw\"](value)); } catch (e) { reject(e); } }\r\n        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }\r\n        step((generator = generator.apply(thisArg, _arguments || [])).next());\r\n    });\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.ComptesController = void 0;\r\nconst common_1 = __webpack_require__(/*! @nestjs/common */ \"@nestjs/common\");\r\nconst jwt_service_1 = __webpack_require__(/*! ../systeme/jwt.service */ \"./src/systeme/jwt.service.ts\");\r\nconst comptes_service_1 = __webpack_require__(/*! ./comptes.service */ \"./src/comptes/comptes.service.ts\");\r\nlet ComptesController = class ComptesController {\r\n    constructor(compteService, jwtService) {\r\n        this.compteService = compteService;\r\n        this.jwtService = jwtService;\r\n    }\r\n    id(id, pass) {\r\n        return __awaiter(this, void 0, void 0, function* () {\r\n            try {\r\n                let response = yield this.compteService.verifie(id, pass);\r\n                return (response);\r\n            }\r\n            catch (error) {\r\n                return (error);\r\n            }\r\n        });\r\n    }\r\n    idErreur() {\r\n        return __awaiter(this, void 0, void 0, function* () {\r\n            throw new common_1.HttpException('LOGIN.NO_CONNECTION', common_1.HttpStatus.UNAUTHORIZED);\r\n            return yield \"Erreur de connexion\";\r\n        });\r\n    }\r\n};\r\n__decorate([\r\n    common_1.Get(':id/:pass'),\r\n    common_1.HttpCode(common_1.HttpStatus.OK),\r\n    __param(0, common_1.Param('id')), __param(1, common_1.Param('pass')),\r\n    __metadata(\"design:type\", Function),\r\n    __metadata(\"design:paramtypes\", [Object, Object]),\r\n    __metadata(\"design:returntype\", Promise)\r\n], ComptesController.prototype, \"id\", null);\r\n__decorate([\r\n    common_1.Get('*'),\r\n    __metadata(\"design:type\", Function),\r\n    __metadata(\"design:paramtypes\", []),\r\n    __metadata(\"design:returntype\", Promise)\r\n], ComptesController.prototype, \"idErreur\", null);\r\nComptesController = __decorate([\r\n    common_1.Controller('comptes'),\r\n    __metadata(\"design:paramtypes\", [comptes_service_1.ComptesService, jwt_service_1.JwtService])\r\n], ComptesController);\r\nexports.ComptesController = ComptesController;\r\n\n\n//# sourceURL=webpack://nemateria-oai-pmh/./src/comptes/comptes.controller.ts?");
+
+/***/ }),
+
+/***/ "./src/comptes/comptes.module.ts":
+/*!***************************************!*\
+  !*** ./src/comptes/comptes.module.ts ***!
+  \***************************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements: top-level-this-exports, __webpack_exports__, __webpack_require__ */
+/*! CommonJS bailout: this is used directly at 2:18-22 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\nvar __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {\r\n    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;\r\n    if (typeof Reflect === \"object\" && typeof Reflect.decorate === \"function\") r = Reflect.decorate(decorators, target, key, desc);\r\n    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;\r\n    return c > 3 && r && Object.defineProperty(target, key, r), r;\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.ComptesModule = void 0;\r\nconst common_1 = __webpack_require__(/*! @nestjs/common */ \"@nestjs/common\");\r\nconst comptes_controller_1 = __webpack_require__(/*! ./comptes.controller */ \"./src/comptes/comptes.controller.ts\");\r\nconst comptes_service_1 = __webpack_require__(/*! ./comptes.service */ \"./src/comptes/comptes.service.ts\");\r\nconst comptes_providers_1 = __webpack_require__(/*! ./comptes.providers */ \"./src/comptes/comptes.providers.ts\");\r\nconst database_module_1 = __webpack_require__(/*! ../db/database.module */ \"./src/db/database.module.ts\");\r\nconst jwt_service_1 = __webpack_require__(/*! ../systeme/jwt.service */ \"./src/systeme/jwt.service.ts\");\r\nlet ComptesModule = class ComptesModule {\r\n};\r\nComptesModule = __decorate([\r\n    common_1.Module({\r\n        imports: [database_module_1.DatabaseModule],\r\n        controllers: [comptes_controller_1.ComptesController],\r\n        providers: [comptes_service_1.ComptesService, ...comptes_providers_1.comptesProviders, jwt_service_1.JwtService]\r\n    })\r\n], ComptesModule);\r\nexports.ComptesModule = ComptesModule;\r\n\n\n//# sourceURL=webpack://nemateria-oai-pmh/./src/comptes/comptes.module.ts?");
+
+/***/ })
+
+};
+exports.runtime =
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => "1e5c84b5a3e9877ea097"
+/******/ 	})();
+/******/ 	
+/******/ }
+;
